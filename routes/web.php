c1903c4a294ab00cb36b0f65c191e2a7cf3fc060
proxycodes <w.pankaj@gmail.com>
2023-02-24 07:58:19 +0530

    3 - set redirects in routes

--- routes/web.php
+++ routes/web.php
@@ -17,8 +17,14 @@ Route::get('/', function () {
     return view('aa-home');
 });
 
+
+Route::get("/contact", function () {
+    return redirect("about"); // contact page should redirect to about as well
+});
+
+
 Route::get('/about', function () {
     return view('about');
 });
 
-Route::view('contact', '/contact');  //the shortcut also works like above about page.
+//Route::view('contact', '/contact');  //the shortcut also works like above about page.
